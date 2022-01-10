c4e6be5f7e00cc5ece6cd477487753157612d234
kenneth <kbybanez@diwamail.com>
2022-01-11 02:15:21 +0800

    find a way to run python in xampp

--- save-assessment-result.php
+++ save-assessment-result.php
@@ -2,18 +2,22 @@
 header('Access-Control-Allow-Origin: *');
 require 'includes/autoloader.php';
 
-use Classes\Database as Database;
-use Classes\Helpers as Helper;
-use Models\AssessmentResults as AssessmentResults;
+// use Classes\Database as Database;
+// use Classes\Helpers as Helper;
+// use Models\AssessmentResults as AssessmentResults;
 
-$db = new Database();
-$helper = new Helper();
-$database = $db->connect();
-$assessmentResult = new AssessmentResults($database);
+// $db = new Database();
+// $helper = new Helper();
+// $database = $db->connect();
+// $assessmentResult = new AssessmentResults($database);
 
-$selectedSubject = $helper->cleanNumber($_POST['selectedSubject']);
-$selectedModule = $helper->cleanNumber($_POST['selectedModule']);
-$selectedSection = $helper->cleanNumber($_POST['selectedSection']);
+// $selectedSubject = $helper->cleanNumber($_POST['selectedSubject']);
+// $selectedModule = $helper->cleanNumber($_POST['selectedModule']);
+// $selectedSection = $helper->cleanNumber($_POST['selectedSection']);
+
+$command_exec = escapeshellcmd('training.py');
+$str_output = shell_exec($command_exec);
+echo var_dump($str_output) ;
 
 /********
  * 
@@ -21,18 +25,18 @@ $selectedSection = $helper->cleanNumber($_POST['selectedSection']);
  * 
 ******end of machine learning section*/
 
-$fileExtension =  pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
-$filePath = "upload/" . 'dindin.' . $fileExtension;
+// $fileExtension =  pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
+// $filePath = "upload/" . 'dindin.' . $fileExtension;
 
-if (move_uploaded_file($_FILES["file"]["tmp_name"], $filePath)) {
-    $assessmentDetails = [
-      'selectedSubject' => $selectedSubject,
-      'creatorId' => 99,
-      'selectedModule' => $selectedModule,
-      'selectedSection' => $selectedSection,
-      'filePath' => $filePath,
-    ];
-    $result = $assessmentResult->insertAssessmentResultDetails($assessmentDetails);
-    echo $result;
-}
+// if (move_uploaded_file($_FILES["file"]["tmp_name"], $filePath)) {
+//     $assessmentDetails = [
+//       'selectedSubject' => $selectedSubject,
+//       'creatorId' => 99,
+//       'selectedModule' => $selectedModule,
+//       'selectedSection' => $selectedSection,
+//       'filePath' => $filePath,
+//     ];
+//     $result = $assessmentResult->insertAssessmentResultDetails($assessmentDetails);
+//     echo $result;
+// }
 ?>
