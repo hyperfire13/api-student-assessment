c12fe40f7fcdfff651f8fe8cbf46c4129b1ea17e
kenneth <kybanez53@gmail.com>
2022-10-05 00:04:10 +0800

    changed api link

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // DB configuration
 define("HOSTTYPE", "mysql");
-define("HOSTNAME", "au77784bkjx6ipju.cbetxkdyhwsb.us-east-1.rds.amazonaws.com");
+define("HOSTNAME", "au77784bkjx6ipju.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306");
 define("USERNAME", "jq2d0y2yr46zrc4n");
 define("PASSWORD", "chonlok1cisv9lye");
 define("DATABASE", "tgzzvim7s0dvj08p");
