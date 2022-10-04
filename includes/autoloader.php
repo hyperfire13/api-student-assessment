31cae17eb60d9e5f94e8e24ecdc7e79b214316a5
kenneth <kybanez53@gmail.com>
2022-10-05 01:39:25 +0800

    changed api link

--- includes/autoloader.php
+++ includes/autoloader.php
@@ -1,17 +1,18 @@
 <?php
-echo dirname(__DIR__) . '\config.php';
-require dirname(__DIR__) . '\config.php';
-spl_autoload_register('classAutoLoader');
+// echo dirname(__DIR__) . '\config.php';
+echo realpath(__DIR__) .'/config.php';
+// require dirname(__DIR__) . '\config.php';
+// spl_autoload_register('classAutoLoader');
 
-function classAutoLoader($className)
-{
-    $paths = array('classes/');
-    $extension = ".php";
-    $fullPath = dirname(__DIR__) . '/' . $className . $extension;
+// function classAutoLoader($className)
+// {
+//     $paths = array('classes/');
+//     $extension = ".php";
+//     $fullPath = dirname(__DIR__) . '/' . $className . $extension;
 
-    if (!file_exists($fullPath)) {
-        return false;
-    }
-    require $fullPath;
-}
+//     if (!file_exists($fullPath)) {
+//         return false;
+//     }
+//     require $fullPath;
+// }
 ?>
