335cd479cc1d69f66076ff94530ba829c1e694c3
hyperfire13 <56341577+hyperfire13@users.noreply.github.com>
2022-10-04 23:11:15 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 // DB configuration
 define("HOSTTYPE", "mysql");
-define("HOSTNAME", "localhost");
-define("USERNAME", "kenneth");
-define("PASSWORD", "123456");
-define("DATABASE", "thesisdb");
+define("HOSTNAME", "au77784bkjx6ipju.cbetxkdyhwsb.us-east-1.rds.amazonaws.com");
+define("USERNAME", "jq2d0y2yr46zrc4n");
+define("PASSWORD", "chonlok1cisv9lye");
+define("DATABASE", "tgzzvim7s0dvj08p");
 define("SSL_KEY", "AES-128-ECB");
 /*** PASSWORD HASH KEY ***/
 define('HASH_PASSWORD_KEY', "machinelearningpython");
